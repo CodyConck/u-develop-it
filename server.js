ec198adf4d6cb2423d1ec0465c917bbb926ca430
CodyConck <codyconck@gmail.com>
2021-06-28 18:19:28 -0500

    queried the database

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const mysql = require('mysql2');
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,12 +8,30 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your MySQL username
+        user: 'root',
+        //Your MySQL password
+        password: 'ju878848!',
+        database: 'election'
+    },
+    console.log('connected to the election database')
+);
+
 //GET test route
 // app.get('/', (req, res) => {
 //     res.json({
 //       message: 'Hello World'
 //     });
 //   });
+
+//query database to test connection
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
   // Default response for any other request (Not Found)
 app.use((req, res) => {
